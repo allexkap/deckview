30b912f3d6d09d375318abe2ff831fc983448fcf
allexkap <aekpstn@gmail.com>
2025-02-12 17:15:12 +0300

    add side panel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+const NAMES: [&str; 3] = ["asd", "qwe", "zxc"];
+
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([1280.0, 800.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Ok(Box::<MyApp>::default())),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+    selected: &'static str,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+            selected: NAMES[0],
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::right("my_left_panel")
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.add_space(8.0);
+                egui::ComboBox::from_id_salt("combo")
+                    .selected_text(format!("{}", self.selected))
+                    .show_ui(ui, |ui| {
+                        for name in NAMES {
+                            ui.selectable_value(&mut self.selected, name, name);
+                        }
+                    });
+                if ui
+                    .add(egui::Button::new("Close").min_size(egui::vec2(100.0, 0.0)))
+                    .clicked()
+                {
+                    ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                }
+            });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            println!("{:?}", ui.available_size());
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
 }
